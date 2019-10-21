738805b6925d1f657121fa4dd123dcb51e5c314e
srinivasa <srinivasa-p@hcl.com>
2019-10-21 12:44:39 +0530

    Initial project setup

---
+++ src/main/java/com/hcl/medicalclaims/MedicalClaimsApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.medicalclaims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalClaimsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalClaimsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/hcl/medicalclaims/exception/GlobalExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.hcl.medicalclaims.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+/**
+ * @author srinivas Used to throw global exceptions
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+	/**
+	 * @param Exception
+	 * @return ResponseEntity
+	 */
+	@ExceptionHandler(CommonException.class)
+	public ResponseEntity<ResponseError> commonException(Exception e) {
+		ResponseError error = new ResponseError(e.getMessage(), HttpStatus.NOT_FOUND.value());
+		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+	}
+
+	/**
+	 * @param exception
+	 * @param request
+	 * @return ResponseEntity
+	 *//*
+		 * @ExceptionHandler(UserExistException.class) public
+		 * ResponseEntity<ResponseError> globalExceptionHandler(UserExistException
+		 * exception, WebRequest request) { ResponseError errorResponse = new
+		 * ResponseError(); errorResponse.setMessage(exception.getMessage());
+		 * errorResponse.setStatusCode(HttpStatus.NOT_FOUND.value()); return new
+		 * ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND); }
+		 */
+
+}
